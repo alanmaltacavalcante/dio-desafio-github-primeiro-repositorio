8b4653127087091d77c3a1a6da836bc2765bf2ff
Alan Cavalcante <alanmaltacavalcante@gmail.com>
2022-08-28 18:14:19 -0300

    Correcao do nome da pasta do site para copia dos arquivos

--- linux-projeto2-iac/script-iac2.sh
+++ linux-projeto2-iac/script-iac2.sh
@@ -23,7 +23,7 @@ echo "Baixando e copiando arquivos do site para o diretório do apache"
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
 echo "Site instalado"
 
